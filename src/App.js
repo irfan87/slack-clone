afe9d88ca91b8bc7cd7bef4bcfccd52e7a4a6b3e
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2020-11-07 16:23:21 +0800

    layout for header, sidebar and sidebar option to showed the channels

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
 	return (
 		<div className="app">
-			<h1>Slack Clone</h1>
+			{/* header */}
+			<Header />
+			<div className="app__body">
+				{/* sidebar */}
+				<Sidebar />
+				{/* react-router -> chat screen */}
+			</div>
 		</div>
 	);
 }
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
+import CreateIcon from "@material-ui/icons/Create";
+import InsertCommentIcon from "@material-ui/icons/InsertComment";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import InboxIcon from "@material-ui/icons/Inbox";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
+import AppsIcon from "@material-ui/icons/Apps";
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add";
+
+import SidebarOption from "../SidebarOption/SidebarOption";
+
+import "./Sidebar.css";
+
+const Sidebar = () => {
+	return (
+		<div className="sidebar">
+			<div className="sidebar__header">
+				<div className="sidebar__info">
+					<h2>ReactJS Coach</h2>
+					<h3>
+						<FiberManualRecordIcon />
+						irfanshukri203
+					</h3>
+				</div>
+				<CreateIcon />
+			</div>
+			<SidebarOption Icon={InsertCommentIcon} title="Threads1" />
+			<SidebarOption Icon={InboxIcon} title="Mentions & Reactions" />
+			<SidebarOption Icon={DraftsIcon} title="Saved Items" />
+			<SidebarOption Icon={BookmarkBorderIcon} title="Channel Browsers" />
+			<SidebarOption Icon={PeopleAltIcon} title="People & User groups" />
+			<SidebarOption Icon={AppsIcon} title="Apps" />
+			<SidebarOption Icon={FileCopyIcon} title="File Browser" />
+			<SidebarOption Icon={ExpandLessIcon} title="Show Less" />
+			<hr />
+			<SidebarOption Icon={ExpandMoreIcon} title="Channels" />
+			<hr />
+			<SidebarOption Icon={AddIcon} title="Channels" />
+			{/* connect to the db and list all the channels */}
+		</div>
+	);
+};
+
+export default Sidebar;
