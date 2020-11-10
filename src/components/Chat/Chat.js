31d662181480f85f783b0d389e124fefa9fb4aee
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2020-11-10 15:07:13 +0800

    retrieved the chat messages from the firebase

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -5,12 +5,15 @@ import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
 import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
 
 import "./Chat.css";
+
 import db from "../../firebaseConfig";
+import Message from "../Message/Message";
 
 const Chat = () => {
 	const { roomId } = useParams();
 
 	const [roomDetails, setRoomDetails] = useState(null);
+	const [roomMessages, setRoomMessages] = useState([]);
 
 	useEffect(() => {
 		if (roomId) {
@@ -20,7 +23,16 @@ const Chat = () => {
 					setRoomDetails(snapshot.data());
 				});
 		}
+		db.collection("rooms")
+			.doc(roomId)
+			.collection("messages")
+			.orderBy("timeStamp", "asc")
+			.onSnapshot((snapshot) =>
+				setRoomMessages(snapshot.docs.map((doc) => doc.data()))
+			);
 	}, [roomId]);
+
+	console.log(roomMessages);
 
 	return (
 		<div className="chat">
@@ -37,6 +49,17 @@ const Chat = () => {
 						Details
 					</p>
 				</div>
+			</div>
+			<div className="chat__messages">
+				{roomMessages.map(({ message, timeStamp, user, userImage }) => (
+					<Message
+						message={message}
+						timeStamp={timeStamp}
+						user={user}
+						userImage={userImage}
+						key={message}
+					/>
+				))}
 			</div>
 		</div>
 	);
---
+++ src/components/Message/Message.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./Message.css";
+
+const Message = ({ message, timeStamp, user, userImage }) => {
+	return (
+		<div className="message">
+			<img src={userImage} alt="" />
+			<div className="message__info">
+				<h4>
+					{user}{" "}
+					<span className="message__timeStamp">
+						{new Date(timeStamp?.toDate()).toUTCString()}
+					</span>
+				</h4>
+				<p>{message}</p>
+			</div>
+		</div>
+	);
+};
+
+export default Message;
